c49706865424ea63eb0e2080deda4bf6186e3d49
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-09-09 02:30:09 +0100

    Drag object to slot minigame

--- Assets/scripts/minigames/MiniGame.cs
+++ Assets/scripts/minigames/MiniGame.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class MiniGame : MonoBehaviour
 {
     public abstract void SetupGame();
 
     public Treasure treasureReward;
+
+    public UnityEvent OnGameComplete;
  
 }
---
+++ Assets/scripts/minigames/PlaceObjectInSlotMiniGame.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Events;
+
+public class PlaceObjectInSlotMiniGame : MiniGame
+{
+
+    [SerializeField] private Vector2[] possibleObjectStarts = new Vector2[4];
+    [SerializeField] private float onSpotTolerance = 0.5f;
+    private float _toleranceCounter = 0;
+
+    public BoxCollider2D slotColldier;
+    public GameObject objectToPut;
+    private bool _objectInSlot = false;
+
+    public override void SetupGame()
+    {
+        int spot = Random.Range(0, possibleObjectStarts.Length - 1);
+        objectToPut.transform.position = possibleObjectStarts[spot];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(slotColldier.OverlapPoint(objectToPut.transform.position))
+        {
+            //In slot
+            _objectInSlot = true;
+        }
+        else
+            _objectInSlot = false;
+
+        if(_objectInSlot)
+        {
+            _toleranceCounter += Time.deltaTime;
+        }
+        else 
+            _toleranceCounter = 0.0f;
+
+        if(_toleranceCounter > onSpotTolerance)
+        {
+            OnGameComplete.Invoke();
+            print("Game won");
+        }
+            
+            
+
+    }
+
+    private void OnDrawGizmosSelected() {
+        for(int i = 0; i < possibleObjectStarts.Length; i++)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(possibleObjectStarts[i],0.5f);
+        }
+
+        if(_objectInSlot)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(slotColldier.transform.position,Vector3.one);
+        }
+    }
+}
