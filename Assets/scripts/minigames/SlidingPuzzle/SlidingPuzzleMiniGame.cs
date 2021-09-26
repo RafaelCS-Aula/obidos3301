16633623582d4a0f2868e3aeeda033107d07368d
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-09-26 01:31:36 +0100

    Board setup and moving tiles, game victory state

--- Assets/scripts/minigames/SlidingPuzzle/SlidingPuzzleMiniGame.cs
+++ Assets/scripts/minigames/SlidingPuzzle/SlidingPuzzleMiniGame.cs
@@ -25,6 +25,15 @@ public class SlidingPuzzleMiniGame : MiniGame
 
 
     public GameObject baseTileButton;
+
+    public bool shuffle = true;
+    private GameObject emptyDummy;
+
+    private void Start()
+    {
+        SetupGame();
+    }
+
     public override void SetupGame()
     {
         boardTiles = new SlidingPuzzleTile[(boardSize * boardSize) - 1];
@@ -36,17 +45,20 @@ public class SlidingPuzzleMiniGame : MiniGame
       
 
         // Populate the board
-        Vector2 emptyTile = new Vector2(boardSize - 1, boardSize - 1);
+        Vector2 emptyTileCoords = new Vector2(boardSize - 1, boardSize-1);
+       // SlidingPuzzleTile emptyTile = new SlidingPuzzleTile(emptyTileCoords, boardSize, nu);
 
         int placedTileCount = 0;
 
+        print(boardSize);
         for(int y = 0; y < boardSize; y++)
         {
             for(int x = 0; x < boardSize; x++)
             {
-                if (x == emptyTile.x && y == emptyTile.y)
+                if (x == emptyTileCoords.x && y == emptyTileCoords.y)
                 {
                     occupationGrid[x, y] = false;
+                    print("Placed empty at: " + x + y);
                     continue;
                 }
                     
@@ -57,31 +69,59 @@ public class SlidingPuzzleMiniGame : MiniGame
                     new Vector2(_boardImages[placedTileCount].width / 2, _boardImages[placedTileCount].height / 2));
 
 
-                SlidingPuzzleTile tile = new SlidingPuzzleTile(new Vector2(x, y));
+                SlidingPuzzleTile tile = new SlidingPuzzleTile(new Vector2(x, y),boardSize,btn);
                 boardTiles[placedTileCount] = tile;
                 btn.GetComponent<Button>().onClick.AddListener(tile.TryMove);
+                btn.name = $"Tile: {tile.gridSolutionPosition} , child Index: {tile.GetChildIndex()}";
                 occupationGrid[x, y] = true;
                 placedTileCount++;
+                
 
             }
 
         }
+
 
         //Shuffle board
-        System.Array.Sort(boardTiles, (x,y) => Random.Range(-1, 1));
 
-        int counter = 0;
-        for(int y = 0; y < boardSize; y++)
+        if (shuffle)
         {
-            for(int x = 0; x < boardSize; x++)
+            System.Array.Sort(boardTiles, (x, y) => Random.Range(-1, 1));
+
+            int counter = 0;
+            for (int y = 0; y < boardSize; y++)
             {
-                boardTiles[0].gridCurrentPosition = new Vector2(x, y);
-                counter++;
+                for (int x = 0; x < boardSize; x++)
+                {
+                    if (counter >= boardTiles.Length)
+                        continue;
+                   // print("shuffling tile at index" + counter);
+                    boardTiles[counter].gridCurrentPosition = new Vector2(x, y);
+
+
+
+                    counter++;
+                }
             }
+
         }
-     
+       
+
+        //Add a dummy empty tile
+        emptyDummy = Instantiate(baseTileButton, this.transform);
+        emptyDummy.GetComponent<Button>().enabled = false;
+        emptyDummy.GetComponent<Image>().enabled = false;
+
+        //Place the Tiles according to their shuffling
+        foreach (SlidingPuzzleTile t in boardTiles)
+        {
+            t.inWorldTile.transform.SetSiblingIndex(t.GetChildIndex());
+            t.emptyDummy = emptyDummy;
+        }
 
     }
+
+
 
 
 
@@ -90,9 +130,23 @@ public class SlidingPuzzleMiniGame : MiniGame
     {
         base.Update();
 
+        if (CheckVictory())
+            WinGame();
+                
+
     }
 
 
+    private bool CheckVictory()
+    {
+        foreach (SlidingPuzzleTile t in boardTiles)
+        {
+            if (t.gridCurrentPosition != t.gridSolutionPosition)
+                return false;
+        }
+
+        return true;
+    }
 
     private Texture2D[] SplitImage(Texture2D fullImage)
     {
@@ -117,4 +171,5 @@ public class SlidingPuzzleMiniGame : MiniGame
         return cells.ToArray();
 
     }
+
 }
--- Assets/scripts/minigames/SlidingPuzzle/SlidingPuzzleTile.cs
+++ Assets/scripts/minigames/SlidingPuzzle/SlidingPuzzleTile.cs
@@ -1,22 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SlidingPuzzleTile 
 {
    // public Texture2D image;
     public Vector2 gridSolutionPosition { get; private set; }
     public Vector2 gridCurrentPosition;
+    public int gridSize { get; private set; }
+    public GameObject inWorldTile;
+    public GameObject emptyDummy;
 
-    public SlidingPuzzleTile(Vector2 solutionPosition)
+    public SlidingPuzzleTile(Vector2 solutionPosition, int gridSize, GameObject inWorldTile)
     {
         gridSolutionPosition = solutionPosition;
+        gridCurrentPosition = gridSolutionPosition;
+        this.gridSize = gridSize;
+        this.inWorldTile = inWorldTile;
+
+        //Debug.Log("Placing Tile at correct position:" + gridSolutionPosition);
     }
 
     public void TryMove()
     {
+        //Debug.Log("Try move on tile at:" + gridCurrentPosition + "child index: " + GetChildIndex());
+        //Debug.Log("Tile at:" + gridCurrentPosition + "has grid spot marked as occupied: " + SlidingPuzzleMiniGame.occupationGrid[(int)gridCurrentPosition.x, (int)gridCurrentPosition.y]);
+
+        Vector2[] neighbourCoords = 
+            {
+            new Vector2(0, 1),
+            new Vector2(1, 0),
+            new Vector2(0, -1),
+            new Vector2(-1, 0) };
+
+        foreach(Vector2 c in neighbourCoords)
+        {
+            Vector2 tentativeCoords = gridCurrentPosition + c;
+            Debug.Log("At " + gridCurrentPosition + "Trying to move to coord:" + tentativeCoords);
+            try
+            {
+                if (SlidingPuzzleMiniGame.occupationGrid[(int)tentativeCoords.x, (int)tentativeCoords.y] == false)
+                {
+                    Debug.Log("Found empty");
+                    // where I am is now empty
+                    SlidingPuzzleMiniGame.occupationGrid[(int)gridCurrentPosition.x, (int)gridCurrentPosition.y] = false;
+
+                    // update coords
+                    emptyDummy.transform.SetSiblingIndex(GetChildIndex());
+                    gridCurrentPosition = tentativeCoords;
+
+                    // where i moved to is now occupied
+                    SlidingPuzzleMiniGame.occupationGrid[(int)gridCurrentPosition.x, (int)gridCurrentPosition.y] = true;
+
+                    // update visuals of the gridLayout component
+                    inWorldTile.transform.SetSiblingIndex(GetChildIndex());
+                    return;
+                }
 
 
+            } catch (IndexOutOfRangeException e) { continue; }
+            
+        }
+
+
+
+    }
+
+    public int GetChildIndex()
+    {
+        //Grid is filled from left to right, bottom to top
+        // assuming grid size 3:
+        // (0,0) is child 0
+        // (1,0) is child 1
+        // (2,0) is child 2
+        // (0,1) is child 3
+        // ...
+
+        int childIndex = (int)(gridCurrentPosition.x + (gridCurrentPosition.y * gridSize));
+        return childIndex;
     }
 
 
